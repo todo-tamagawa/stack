2eb9af09b9988fc06be8c20f941cb5810d350ec9
k.tamagawa <tamagawa@nikelanddime.jp>
2017-09-07 11:59:58 +0900

    fix

--- ix/test/Ch01/MinfreeSpec.hs
+++ ix/test/Ch01/MinfreeSpec.hs
@@ -6,9 +6,9 @@ import Ch01.Minfree
 spec :: Spec
 spec = do
   describe "minfree" $ do
-    it "本に載っている例" $ do
+    it "one" $ do
       minfree [8,23,9,0,12,11,1,10,13,7,41,4,14,21,5,17,3,19,2,6] `shouldBe` 15
 
   describe "minfree'" $ do
-    it "本に載っている例" $ do
+    it "two" $ do
       minfree' [8,23,9,0,12,11,1,10,13,7,41,4,14,21,5,17,3,19,2,6] `shouldBe` 15
